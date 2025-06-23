93e9dc5b2c0af2f4d28a37cf46fd90d12299cc7b
Tarun Yaduwanshi <tarunyaduwanshi0@gmail.com>
2025-06-23 13:42:26 +0530

    spelling mistake

--- frontend/src/pages/ItemPage.jsx
+++ frontend/src/pages/ItemPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import Carousel from '../../components/carousel';
+import Carousel from '../../components/Carousel';
 
 
 const ItemPage = () => {
